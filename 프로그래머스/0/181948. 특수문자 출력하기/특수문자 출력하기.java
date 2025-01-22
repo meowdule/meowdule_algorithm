7de1372dfc68d463012ad0415f743edfc803a4b9
meowdule <meowdule73@gmail.com>
2025-01-22 23:49:58 +0900

    [level 0] Title: 특수문자 출력하기, Time: 125.36 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // ""와 ''의 출력을 위해 \\ 사용하기
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+}
