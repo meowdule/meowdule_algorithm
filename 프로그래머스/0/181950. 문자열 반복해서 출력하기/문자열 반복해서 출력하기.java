d7ff316941f7078af7050f63c413d14f4b1f87e0
meowdule <meowdule73@gmail.com>
2025-01-22 23:50:48 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 192.03 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -5,7 +5,7 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int n = sc.nextInt();
-        while (n > 0) {
+        while (n > 0) {                         // 반복문을 반복하며 n은 1씩 줄어들고 0이되면 반복 종료
             System.out.print(str);
             n -= 1;
         }
