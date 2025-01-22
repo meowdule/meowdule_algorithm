328817a94e53bd5a054e505363c140aaccc185aa
meowdule <meowdule73@gmail.com>
2025-01-22 23:51:16 +0900

    [level 0] Title: 문자열 출력하기, Time: 162.70 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -4,7 +4,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        System.out.println(a);
+        System.out.println(a);                      // 출력 : print
         
     }
 }
