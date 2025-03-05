d4a925e0c885757e982dae353a4e193d053c61cb
meowdule <meowdule73@gmail.com>
2025-03-05 16:24:58 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,7 +1,3 @@
 def solution(numbers):
-    max_num = numbers[0] * numbers[1]
-    for num1 in numbers:
-        for num2 in numbers:
-            if num1 != num2 and num1 * num2 > max_num:
-                max_num = num1 * num2
-    return max_num
+    numbers = sorted(numbers)
+    return max(numbers[-1] * numbers[-2] , numbers[0] * numbers[1])
