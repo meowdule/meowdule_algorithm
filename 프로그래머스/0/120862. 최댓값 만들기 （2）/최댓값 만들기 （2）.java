745dc735f2afdb32c88162a198cd828295028ebc
meowdule <meowdule73@gmail.com>
2025-03-05 16:29:59 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.54 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -5,6 +5,6 @@ class Solution {
         Arrays.sort(numbers);
         int max_num1 = numbers[numbers.length - 1] * numbers[numbers.length - 2];
         int max_num2 = numbers[1] * numbers[0];
-        return (max_num1 > max_num2) ? max_num1 : max_num2;
+        return Math.max(max_num1 , max_num2);
     }
 }
