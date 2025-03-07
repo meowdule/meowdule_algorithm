af99ba6c53ab7e76e4c236aa643737e88cf8921f
meowdule <meowdule73@gmail.com>
2025-03-07 12:43:14 +0900

    Update 삼각형의 완성조건 -（2）.java

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 -（2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 -（2）.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int[] sides) {
-        int answer = 0;
-        int max = Math.max(sides[0], sides[1]);
-        int min = Math.min(sides[0], sides[1]);
-
-        answer += min * 2 - 1;
-
+        int answer = (Math.min(sides[0], sides[1])) * 2 - 1;
         return answer;
     }
 }
